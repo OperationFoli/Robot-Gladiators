6ecd7e10b27bc0b71fd92a92c242a75038083532
Foli <ross.joseph10@yahoo.com>
2022-08-06 15:49:54 -0400

    testing console log

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,1 +1,23 @@
-window.alert("This is an alert! Javascript is running");
+var playerName = window.prompt("What is your robots's name?");
+// Note the lack of quotation marks around playerName
+console.log(playerName);
+
+console.log("This logs a string, good for leaving yourself a mesasge");
+
+// this will do math and log 20
+console.log(10 + 10);
+
+// this will concatenate
+console.log("Our robot name is " + playerName);
+
+function fight() {
+    window.alert("The fight has begun!");
+}   
+
+// fight();
+
+var stringDataType = "This is a string, which is a fancy way to say text";
+var numberIntergerDataType = 10;
+var numberFloatDataType = 10.4;
+var booleanDataType = true;
+
